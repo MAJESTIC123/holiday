52a4fc308d50af596f74447c91a3b713d156acb8
MAJESTIC123 <karthiece996@gmail.com>
2017-06-14 14:34:48 +0530

    Add files via upload

---
+++ holiday.java
@@ -0,0 +1,40 @@
+public class days {
+    public static void main(String[] args) {
+
+       int a=7;
+      String dayString=["monday","tuesday","wednesday","thursday","friday","saturday","sunday"];
+        switch (a) {
+            case 1:  
+                  System.out.println("true");
+                     break;
+            case 2:  
+System.out.println("true");
+                     break;
+            case 3:  
+System.out.println("true");
+                     break;
+            case 4: 
+System.out.println("true");
+                     break;
+            case 5: 
+System.out.println("true");
+                     break;
+            case 6:  
+System.out.println("false");
+                     break;
+            case 7:  
+System.out.println("false");
+                     break;
+            
+          
+            default: dayString = "Invalid day";
+                     break;
+        }
+       
+if(a=>1;a=<5;a++)
+{
+System.out.println("true");
+}
+else
+System.out.println("false");
+}
